8182a3903bf9ac7ddddac6e7632f5a97544c6fd3
Sazib <mahiburislamsazib@gmail.com>
2024-06-27 20:36:21 +0600

    task-3

---
+++ task-3.js
@@ -0,0 +1,13 @@
+var firstCont ="I am going to ";
+var firstCont2 = "be";
+var cont = firstCont + ' ' + firstCont2;
+console.log(cont)
+
+var secondCont = "an awesome web developer of the contain the";
+var secondCont2 = "strings";
+var container = secondCont + ' ' + secondCont2;
+
+console.log(container)
+
+console.log(cont + ' ' + container)
+
